b1750369e8e84a2efeef64b411ac550879b35330
Xiao Jiang <bjjx1999@live.com>
2018-07-27 20:43:00 +0800

    Regular

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,16 @@ using UnityEditor.Animations;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-	public Transform playerTransform;
+//	public Transform playerTransform;
 
 //	private Camera camera;
 	private Animator animator;
-	private GameObject player;
-	private PlayerController playerController;
+//	private GameObject player;
+//	private PlayerController playerController;
 	private CameraStatus status;
-	private Vector2 desiredVelocity;
-	private Rigidbody2D rigi;
-	private Vector3 velocity = Vector3.zero;
+//	private Vector2 desiredVelocity;
+//	private Rigidbody2D rigi;
+//	private Vector3 velocity = Vector3.zero;
 
 	public void PlayerDieAnimation() {
 		animator.SetTrigger("Die");
@@ -22,45 +22,12 @@ public class CameraController : MonoBehaviour {
 	
 
 	private void OnEnable() {
-		rigi = GetComponent<Rigidbody2D>();
+//		rigi = GetComponent<Rigidbody2D>();
 		animator = GetComponent<Animator>();
-//		camera = GetComponent<Camera>();
-		player = GameObject.FindGameObjectWithTag("Player");
-		playerController = player.GetComponent<PlayerController>();
-		playerTransform = GameObject.Find("Player/PlayerCenterReferencePoint").transform;
-//		Debug.Log(playerTransform.position);
-//		Follow(playerTransform.position, 5f);
-//		Vector2.SmoothDamp(transform.position, playerTransform.position, ref desiredVelocity, 5f);
+//		player = GameObject.FindGameObjectWithTag("Player");
+//		playerController = player.GetComponent<PlayerController>();
+//		playerTransform = GameObject.Find("Player/PlayerCenterReferencePoint").transform;
 	}
 
-	private void Update () {
-//		transform.position = player.transform.position + Vector3.back;
-//		Follow(playerTransform, 0.1f);
-//		CalculateAndMove();
-	}
 
-//	private void CalculateAndMove() {
-//		float a = playerController.GetStatus().acceleration;
-//		bool accelerationIsNegative = a < 0;
-//		float d = Mathf.Log10(Mathf.Abs(a) + 1);
-//		float angle = Vector2.Angle(Vector2.left, playerTransform.position - transform.position);
-//		float dx = d * Mathf.Cos(Mathf.Deg2Rad * angle);
-//		float dy = d * Mathf.Sin(Mathf.Deg2Rad * angle);
-//		if (accelerationIsNegative) {
-//			transform.position = new Vector3(playerTransform.position.x + dx, playerTransform.position.y + dy, -10f);
-//		}
-//		else {
-//			transform.position = new Vector3(playerTransform.position.x - dx, playerTransform.position.y - dy, -10f);
-//		}
-//	}
-
-//	void Follow(Transform target, float dampTime) {
-//		if (target)
-//		{
-//			Vector3 point = camera.WorldToViewportPoint(target.position);
-//			Vector3 delta = target.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-//			Vector3 destination = transform.position + delta;
-//			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-//		}
-//	}
 }
---
+++ Assets/Scripts/FollowPlayerVC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerVC : MonoBehaviour {
+	public Transform playerTransform;
+	public float rotationDeltaMultiplier;
+	
+	private float rotationDelta = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float angle = Quaternion.Angle(transform.rotation, playerTransform.rotation);
+		rotationDelta = angle / 10f * rotationDeltaMultiplier;
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, playerTransform.rotation, rotationDelta);
+//		transform.rotation = Quaternion.Euler(0f, 0f, 50f);
+	}
+	
+}
